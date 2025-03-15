a8ec51dcbe23a0884dedc784746804d671ec12a4
Halil ibrahim Kes <ibrahim.kes@cgbilgi.com>
2025-03-15 19:16:38 +0300

    Update discordBot.py

--- discordBot.py
+++ discordBot.py
@@ -22,7 +22,7 @@ async def heh(ctx, count_heh = 5):
 
 @bot.command()
 async def joined(ctx, member: discord.Member):
-    """Says when a member joined."""
+    """ Kişinin ne zaman giriş yaptığını gösterir."""
     await ctx.send(f'{member.name} joined {discord.utils.format_dt(member.joined_at)}')
 
 @bot.command()
@@ -36,11 +36,6 @@ async def roll(ctx, dice: str):
 
     result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
     await ctx.send(result)
-
-@bot.command()
-async def add(ctx, left: int, right: int):
-    """Adds two numbers together."""
-    await ctx.send(left + right)
 
 
 bot.run("")

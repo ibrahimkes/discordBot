fc939fd075b892ff281a443c3cde8b279c519548
Halil ibrahim Kes <ibrahim.kes@cgbilgi.com>
2025-03-25 20:39:12 +0300

    Yeni yapılan güncelleme

--- discordBot.py
+++ discordBot.py
@@ -7,18 +7,14 @@ intents = discord.Intents.default()
 intents.message_content = True
 
 bot = commands.Bot(command_prefix='.', intents=intents)
-
+# Yorum satırı yeni eklendi
 @bot.event
 async def on_ready():
-    print(f'{bot.user} olarak giriş yaptık')
+    print(f'{bot.user} olarak giriş yapıldı.')
 
 @bot.command()
 async def hello(ctx):
     await ctx.send(f'Merhaba! Ben {bot.user}, bir Discord sohbet botuyum!')
-
-@bot.command()
-async def heh(ctx, count_heh = 5):
-    await ctx.send("he" * count_heh)
 
 @bot.command()
 async def joined(ctx, member: discord.Member):
